738a43e9ef2460f668393888475cae273c85a1be
matias <mati.carvajal.caaa@gmail.com>
2021-08-12 20:02:06 -0400

    f

--- apiServices/get/controller.js
+++ apiServices/get/controller.js
@@ -5,10 +5,6 @@ module.exports = {
     async readMaker(req, res){
         const maker = await getModel.readMaker(req.body);
 
-        //var resultado = await requestify.post('http://localhost:3000/get/maker', {id: 1}).then((response) =>{
-        //    console.loog(response.getBody());
-        //});
-
         return res.json(maker);
     },
 
